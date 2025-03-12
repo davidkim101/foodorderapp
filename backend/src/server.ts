4766ea43ff8caefe0071f6867100f4ad1378a8fa
David Kim <davidkim.101@gmail.com>
2025-03-11 19:16:09 -0500

    update frontend url

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:4200", "https://foodexpress-yq9e.onrender.com"],
+    origin: ["https://foodexpress-yq9e.onrender.com"],
   })
 );
 
